71e42340655afe0ed1d475559afa488a7620e7c6
Alex <alex@a.com>
2024-08-04 20:49:48 +0300

    Fixed update_password

--- internal/services/update_password.go
+++ internal/services/update_password.go
@@ -2,18 +2,19 @@ package services
 
 import (
 	"fmt"
+	"password_manager/internal/db"
 	"password_manager/pkg/input"
 )
 
 func UpdatePassword(title string) {
-	password, found := GetPassword(title)
-	if !found {
-		fmt.Println("Password not found.")
-		return
+	for i, password := range db.Passwords {
+		if password.Title == title {
+			fmt.Print("Enter the new password: ")
+			value := input.GetInput()
+			db.Passwords[i].Value = value
+			fmt.Println("Password updated successfully!")
+			return
+		}
 	}
-	fmt.Print("Enter the new password: ")
-	value := input.GetInput()
-
-	password.Value = value
-	fmt.Println("Password updated successfully!")
+	fmt.Println("Password not found.")
 }
